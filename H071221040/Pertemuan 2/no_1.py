2513c5c1c6e787ff2ecd7bd394e572a42b3f266a
alba054 <jralif02@gmail.com>
2022-09-25 20:31:02 +0800

    kumpul tugas 2

---
+++ H071221040/Pertemuan 2/no_1.py
@@ -0,0 +1,14 @@
+skor_siswa = float(input('masukkan skor ='))
+
+if skor_siswa>=40 :
+    print (f'nilai {skor_siswa} = "E" ')
+elif skor_siswa>=80 :
+    print (f'nilai {skor_siswa} = "B" ')
+elif skor_siswa>=70 :
+    print (f'nilai {skor_siswa} = "C" ')
+elif skor_siswa>=60 :
+    print (f'nilai {skor_siswa} = "D" ')
+elif skor_siswa>=90 :
+    print (f'nilai {skor_siswa} = "A" ')
+else :
+    print (f'nilai {skor_siswa} = "F" ')
