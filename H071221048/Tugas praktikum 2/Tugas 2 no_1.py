fbb877f21676b818ce2b40a8150b602c3f8306cb
AhmadSalwan <salwanfarid@gmail.com>
2022-09-27 13:57:45 +0800

    kumpul tugas 2

---
+++ H071221048/Tugas praktikum 2/Tugas 2 no_1.py
@@ -0,0 +1,32 @@
+n=int(input("Nilai="))
+if n >=90:
+    print(">nilai ",n,"A")
+elif n >=80:
+    print(">nilai",n,"B")
+elif n >=70:
+    print(">nilai",n,"C")
+elif n>=60:
+    print(">nilai",n,"D")
+elif n>=40:
+    print("nilai",n,"E")
+else:
+    print("nilai ",n,"F")
+
+# n = int(input())
+
+# if n > 1:
+#     print("lebih besar dari 1")
+# if n > 5:
+#     print("lebih besar dari 5")
+# elif n > 3:
+#     print("lebih besar dari 3")
+# else:
+#     print("lebih besar dari 2")
+
+# n = 3
+# if n % 2 == 1:
+#     #ganjil
+#     pass
+# elif n % 2 == 0:
+#     #genap
+#     pass
