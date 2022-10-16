20a24b658e9e511c19849feb5bdb298f8baa86cf
ayuwidianti13 <113836478+ayuwidianti13@users.noreply.github.com>
2022-10-16 17:16:56 +0800

    Delete no3.py

--- H071221056/Tugas praktikum 5/no3.py
+++
@@ -1,8 +0,0 @@
-array1 = list(map(int,input("input array ke 1: ").split(" ")))
-array2 = list(map(int,input("input array ke 2: ").split(" ")))
-irisan = []
-for i in array2:
-   if i in array1: 
-      irisan.append(i)
-arrays = tuple(irisan)
-print(f"terdapat {len(irisan)} duplikat yaitu : {arrays}")
