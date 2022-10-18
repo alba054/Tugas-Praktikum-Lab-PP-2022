adfa7cf1d95d3f9180cc086da2844a0adc9d90b5
maykoRa <maykoraditya34@gmail.com>
2022-10-18 19:03:31 +0800

    kumpul tugas 5

---
+++ H071221096/Pertemuan 5/no3.2.py
@@ -0,0 +1,10 @@
+arr1 = list(input("masukkan array 1: ").split())
+arr2 = list(input("masukkan array 2: ").split())
+
+arr3 = []
+
+for i in arr1:
+    for j in arr2:
+        if i == j:   
+            arr3.append(j)
+print(arr3)
---
+++ H071221096/Pertemuan 5/no3.py
@@ -0,0 +1,5 @@
+x = set(input("Input array ke 1: ").split())
+y = set(input("Input array ke 2: ").split())
+
+a = x.intersection(y)
+print("Terdapat 2 buah duplikat yaitu" , (a))
