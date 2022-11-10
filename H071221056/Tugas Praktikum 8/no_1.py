addcb4c1a77a4f62e47738d09fd1fc1de77cdb17
ayuwidianti <ayuwdnti13@gmail.com>
2022-11-10 16:48:53 +0800

    kumpul tugas

---
+++ H071221056/Tugas Praktikum 8/no_1.py
@@ -0,0 +1,46 @@
+# creat class
+class person:
+
+    def __init__(self, name, age, gender, alamat):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.alamat = alamat
+    
+    def setAge(self, age) :
+        self.age = age
+    def getAge(self):
+        return self.age
+    def setName(self, name) :
+        self.name = name
+    def getName(self):
+        return self.name
+    def setGender(self, gender) :
+        self.gender = gender
+    def getGender(self):
+        if self.gender == True :
+            return "Male"
+        elif self.gender == False:
+
+            return "Female"
+        else :
+            return "gender harus true atau False"
+    def setAlamat(self, alamat) :
+        self.alamat = alamat
+    def getAlamat (self) :
+        return self.alamat
+    def setdefaultAge(self):
+        self.setAge(10)
+
+
+data = person("Ayu", 18, False, "makassar")
+print(data.getAge())
+print(data.getName())
+print(data.getGender())
+print(data.getAlamat())
+data.setName("lisa")
+data.setAge(10)
+data.setGender(3)
+print(data.getGender())
+data.name = "nakita"
+print(data.getName())
